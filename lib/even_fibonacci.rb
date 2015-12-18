2a330cd39baf294eb8c0beac824247695d0f375f
rluftw <richuuuurd@gmail.com>
2015-12-18 12:36:17 -0500

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,24 @@
 # Implement your procedural solution here!
+
+def fibonacci(index)
+	if index == 0
+		0
+	elsif index == 1
+		1
+	else 
+		fibonacci(index-1) + fibonacci(index-2)
+	end
+end
+
+def even_fibonacci_sum(amount)
+	sum, counter = 0, 2
+	loop do
+		fib = fibonacci(counter)
+		break if fib > amount
+		if fib % 2 == 0 
+			sum += fib
+		end
+		counter += 1
+	end
+	sum
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,30 @@
 # Implement your object-oriented solution here!
+
+class EvenFibonacci
+	def initialize(amount)
+		@amount = amount
+	end
+
+	def fibonacci(index)
+		if index == 0
+			0
+		elsif index == 1
+			1
+		else 
+			fibonacci(index-1) + fibonacci(index-2)
+		end
+	end
+
+	def sum
+		sum, counter = 0, 2
+		loop do
+			fib = fibonacci(counter)
+			break if fib > @amount
+			if fib % 2 == 0 
+				sum += fib
+			end
+			counter += 1
+		end
+		sum
+	end
+end
